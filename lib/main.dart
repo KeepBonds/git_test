ccabf9d90132abdc3a1fb689781639fd00da99e3
Edwin <edwin@ragic.com>
2024-02-14 12:55:47 +0800

    change to dev

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     print('test branch');
+    print("change to dev");
     print("first modif dev branch");
     super.initState();
   }
